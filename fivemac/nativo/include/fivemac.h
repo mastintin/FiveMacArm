b77d9263fec6013f3c7ff350ad015ca5cc11e842
Manuel Alvarez Sanchez <mastintin@gmail.com>
2026-02-22 17:06:00 +0100

    Estableciendo punto de restauración y nuevos tests

--- fivemac/nativo/include/fivemac.h
+++ fivemac/nativo/include/fivemac.h
@@ -3,6 +3,7 @@
 #include "fmsgs.h"
 #include <hbapi.h>
 #include <hbapifs.h>
+#include <hbapiitm.h>
 #include <hbvm.h>
 
 #define RGB(nRed, nGreen, nBlue) (nRed + (nGreen * 256) + (nBlue * 65536))
@@ -26,3 +27,40 @@ void ValToChar(PHB_ITEM item);
 void ImgResize(NSImage *image, int nWidth, int nHeight);
 
 NSImage *ImgTemplate(NSString *);
+
+#define FIVEMAC_DRAGDROP_METHODS                                               \
+  - (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender {               \
+    return NSDragOperationCopy;                                                \
+  }                                                                            \
+  - (NSDragOperation)draggingUpdated:(id<NSDraggingInfo>)sender {               \
+    return NSDragOperationCopy;                                                \
+  }                                                                            \
+  - (BOOL)performDragOperation:(id<NSDraggingInfo>)sender {                     \
+    NSPasteboard *pboard = [sender draggingPasteboard];                        \
+    NSArray *classes = [NSArray arrayWithObject:[NSURL class]];                \
+    NSDictionary *options = [NSDictionary dictionary];                         \
+    NSArray *fileURLs = [pboard readObjectsForClasses:classes                  \
+                                              options:options];                \
+    if (fileURLs != nil && [fileURLs count] > 0) {                             \
+      PHB_SYMB symFMH = hb_dynsymSymbol(hb_dynsymFindName("_FMH"));            \
+      if (symFMH) {                                                            \
+        hb_vmPushSymbol(symFMH);                                               \
+        hb_vmPushNil();                                                        \
+        hb_vmPushNumInt((HB_LONGLONG)[self window]);                           \
+        hb_vmPushLong(WM_DROPFILES);                                           \
+        hb_vmPushNumInt((HB_LONGLONG)self);                                    \
+        PHB_ITEM pArray = hb_itemArrayNew([fileURLs count]);                   \
+        for (NSUInteger i = 0; i < [fileURLs count]; i++) {                    \
+          PHB_ITEM pString = hb_itemPutC(                                      \
+              NULL, [[[fileURLs objectAtIndex:i] path] UTF8String]);           \
+          hb_itemArrayPut(pArray, i + 1, pString);                             \
+          hb_itemRelease(pString);                                             \
+        }                                                                      \
+        hb_vmPush(pArray);                                                     \
+        hb_vmDo(4);                                                            \
+        hb_itemRelease(pArray);                                                \
+      }                                                                        \
+      return YES;                                                              \
+    }                                                                          \
+    return NO;                                                                 \
+  }
